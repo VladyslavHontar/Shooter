54598a77d42784fcf98ebfd642721b77e9e84cee
Vlad <hontarvladie@gmail.com>
2023-09-09 18:55:52 +0100

    .

--- src/main/Window.java
+++ src/main/Window.java
@@ -7,8 +7,6 @@ public class Window extends JFrame {
   public Window(GPanel panel) {
     super("Window");
     setUndecorated(true);
-    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    setSize(screenSize.width, screenSize.height);
     setExtendedState(JFrame.MAXIMIZED_BOTH);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     add(panel);
